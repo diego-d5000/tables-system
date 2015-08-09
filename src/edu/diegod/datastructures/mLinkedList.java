b1225a9933b3d2b648cac159c549160598b8cecf
Diego Plascencia <diego-d@outlook.com>
2015-08-08 22:37:24 -0500

    added interface and implementation of LinkedList data structure

---
+++ src/edu/diegod/datastructures/mLinkedList.java
@@ -0,0 +1,89 @@
+package edu.diegod.datastructures;
+
+import java.util.List;
+
+/**
+ * Created by diego-d on 8/7/15.
+ */
+public class mLinkedList implements mList {
+    private Node head;
+    private int listCount;
+
+    public mLinkedList() {
+        head = null;
+        listCount = 0;
+    }
+
+    @Override
+    public void add(Object data) {
+        Node addedNode;
+        addedNode = new Node(data, listCount);
+        addedNode.linkedTo = head;
+        head = addedNode;
+        listCount++;
+    }
+
+    @Override
+    public boolean remove(int index) {
+        Node current, prev;
+        boolean found;
+
+        current = head;
+        prev = null;
+        found = false;
+
+        while ((current != null) && (!found)) {
+            found = (current.index == index);
+            if (!found) {
+                prev = current;
+                current = current.linkedTo;
+            }
+            if (current != null) {
+                if (current == head)
+                    head = current.linkedTo;
+                else
+                    prev.linkedTo = current.linkedTo;
+            }
+            current = null;
+        }
+        return found;
+    }
+
+    @Override
+    public Object get(int index) {
+        Node result;
+        for (result = head; result != null; result = result.linkedTo)
+            if (result.index == index)
+                return result;
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return listCount;
+    }
+
+    private class Node {
+        Object data;
+        Node linkedTo;
+        int index;
+
+        public Node(Object data, int index) {
+            this.data = data;
+            this.linkedTo = null;
+            this.index = index;
+        }
+
+        public Object getData() {
+            return data;
+        }
+
+        public Node getLinkedTo() {
+            return linkedTo;
+        }
+
+        public void setLinkedTo(Node linkedTo) {
+            this.linkedTo = linkedTo;
+        }
+    }
+}
---
+++ src/edu/diegod/datastructures/mList.java
@@ -0,0 +1,14 @@
+package edu.diegod.datastructures;
+
+/**
+ * Created by diego-d on 8/7/15.
+ */
+public interface mList {
+    void add(Object data);
+
+    boolean remove(int index);
+
+    Object get(int index);
+
+    int size();
+}
